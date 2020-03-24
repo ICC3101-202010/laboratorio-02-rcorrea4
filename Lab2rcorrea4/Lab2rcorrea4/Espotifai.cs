a6c85b69c822e967033ef3e930a08177e81df210
rcorrea4 <rcorrea4@miuandes.cl>
2020-03-24 17:15:42 -0300

    second commit

--- Lab2rcorrea4/Lab2rcorrea4/Espotifai.cs
+++ Lab2rcorrea4/Lab2rcorrea4/Espotifai.cs
@@ -12,5 +12,31 @@ namespace Lab2rcorrea4
         {
 
         }
+        public bool AgregarCancion(Cancion cancion)
+        {
+            int i = canciones.Length;
+            bool repetida= false;
+            for (int o = 0; o <= canciones.Length; o++)
+            {
+                if (canciones[o] == cancion)
+                {
+                    repetida= true;
+                }
+               
+            }
+            if (repetida == true)
+            {
+                return false;
+            }
+            else
+            {
+                canciones[i + 1] = cancion;
+                return true;
+
+            }
+
+
+
+        }
     }
 }
